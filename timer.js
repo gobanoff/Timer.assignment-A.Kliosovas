b71f46f61ea409fc296992a69c9b34126ea68476
gobanoff <a.kliosovas@yahoo.com>
2023-08-05 16:28:48 +0300

    first commit

---
+++ timer.js
@@ -0,0 +1,18 @@
+let finish;
+function reset() {
+    clearInterval(finish);
+    document.getElementById("min").value = "";
+    document.getElementById("countdown").textContent = "00:00";
+  }
+
+
+
+
+
+
+
+
+
+
+
+

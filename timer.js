616ad2a9e6b4c289668ff5891f85b7b545c7f98a
gobanoff <a.kliosovas@yahoo.com>
2023-08-05 17:00:12 +0300

    second commit

--- timer.js
+++ timer.js
@@ -4,7 +4,12 @@ function reset() {
     document.getElementById("min").value = "";
     document.getElementById("countdown").textContent = "00:00";
   }
-
+  function displayTime(seconds) {
+    const min = Math.floor(seconds / 60);
+    const sec = seconds % 60;
+    const form = `${String(min).padStart(2, "0")}:${String(sec).padStart( 2, "0")}`;
+    document.getElementById("countdown").textContent = form;
+  }
 
 
 
